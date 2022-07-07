6f484c48868393843aff92ad9b3493ac7843922c
+ <escobedo.jose48@outlook.com>
2022-07-07 00:07:25 -0700

    gallery component rendering one image with Static Image adding images to images folder

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,7 +1,26 @@
 import React from "react"
-
+import { StaticImage } from "gatsby-plugin-image"
 const gallery = () => {
-  return <div>gallery</div>
+  return (
+    <div>
+      <StaticImage
+        style={{
+          // You can set a maximum height for the image, if you wish.
+          maxHeight: 600,
+        }}
+        layout="fixed"
+        width={200}
+        height={200}
+        // You can optionally force an aspect ratio for the generated image
+        aspectRatio={1 / 1}
+        // This is a presentational image, so the alt should be an empty string
+        alt="landscape-mtn"
+        // Assisi, Perúgia, Itália by Bernardo Ferrari, via Unsplash
+        src="../images/project-4.jpg"
+        formats={["auto", "webp", "avif"]}
+      />
+    </div>
+  )
 }
 
 export default gallery
